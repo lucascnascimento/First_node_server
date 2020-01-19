db7c69a9ccf844dc33984e6f6d20060e0515122c
Lucas Cordeiro <lucascnascimento@gmail.com>
2020-01-19 20:00:04 -0300

    First try to make the post request

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+const projects = [];
+
+server.post("/projects", (req, res) => {
+  projects.push(req.body);
+  return res.json(projects);
+});
+
+// server.get("/projects", (req, res) => {
+//   return res.json("hello res");
+// });
+
+// server.delete("/projects", (req, res) => {
+//   return res.json("hello res");
+// });
+
+// server.update("/projects", (req, res) => {
+//   return res.json("hello res");
+// });
+
+server.listen(3000);
