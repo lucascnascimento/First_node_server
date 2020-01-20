c4af0e9933cbcbdccd59a613d6e2110e240f92eb
Lucas Cordeiro <lucascnascimento@gmail.com>
2020-01-20 20:57:42 -0300

    Post new task to project added

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ server.use(express.json());
 
 const projects = [];
 
+// Adds a new Project
 server.post("/projects", (req, res) => {
   projects.push(req.body);
   return res.json(projects);
 });
 
+// Get all projects
 server.get("/projects", (req, res) => {
   return res.json(projects);
 });
 
+// Changes project title
 server.put("/projects/:id", (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
@@ -26,6 +29,7 @@ server.put("/projects/:id", (req, res) => {
   return res.json(projects);
 });
 
+// Delete a project
 server.delete("/projects/:id", (req, res) => {
   const { id } = req.params;
 
@@ -36,12 +40,16 @@ server.delete("/projects/:id", (req, res) => {
   return res.json(projects);
 });
 
-// server.delete("/projects", (req, res) => {
-//   return res.json("hello res");
-// });
+// Adds a task to a project
+server.post("/projects/:id/tasks", (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
 
-// server.update("/projects", (req, res) => {
-//   return res.json("hello res");
-// });
+  projectIndex = projects.findIndex(obj => obj.id == id);
+
+  projects[projectIndex].tasks.push(title);
+
+  return res.json(projects);
+});
 
 server.listen(3000);
