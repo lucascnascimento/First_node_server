cdfdc969572a13b508d989d2aa8ff67ef9a95cd2
Lucas Cordeiro <lucascnascimento@gmail.com>
2020-01-21 18:31:37 -0300

    Implemented middleware that count how many requests were made

--- index.js
+++ index.js
@@ -6,19 +6,38 @@ server.use(express.json());
 
 const projects = [];
 
+let count = 0;
+
+// Middleware for id validation
+function checkIdExists(req, res, next) {
+  // if (projects.findIndex(obj => obj.id == req.params) == -1) {
+  //   return res.status(400).json({ error: "Project id does not exist" });
+  // }
+
+  return next();
+}
+
+// Middleware that counts how many requests were made
+function countRequest(req, res, next) {
+  count++;
+  console.log(`Requests made: ${count}`);
+
+  return next();
+}
+
 // Adds a new Project
-server.post("/projects", (req, res) => {
+server.post("/projects", countRequest, (req, res) => {
   projects.push(req.body);
   return res.json(projects);
 });
 
 // Get all projects
-server.get("/projects", (req, res) => {
+server.get("/projects", countRequest, (req, res) => {
   return res.json(projects);
 });
 
 // Changes project title
-server.put("/projects/:id", (req, res) => {
+server.put("/projects/:id", checkIdExists, countRequest, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
 
@@ -30,7 +49,7 @@ server.put("/projects/:id", (req, res) => {
 });
 
 // Delete a project
-server.delete("/projects/:id", (req, res) => {
+server.delete("/projects/:id", checkIdExists, countRequest, (req, res) => {
   const { id } = req.params;
 
   projectIndex = projects.findIndex(obj => obj.id == id);
@@ -41,7 +60,7 @@ server.delete("/projects/:id", (req, res) => {
 });
 
 // Adds a task to a project
-server.post("/projects/:id/tasks", (req, res) => {
+server.post("/projects/:id/tasks", checkIdExists, countRequest, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
 
